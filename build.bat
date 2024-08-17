a0ebe6e40b6e454421961a0771606f5ba5d77325
Asher Max Schweigart <AsherMaximum@gmail.com>
2024-08-17 05:25:59 -0400

    Updating manifest with url and switching build to a mcpack

--- build.bat
+++ build.bat
@@ -1,23 +1,20 @@
 @echo off
 setlocal
 
-:: Get the name of the batch file without extension
-set "batchFileName=%~nx0"
-
 :: Set the final file name
-set "addonFileName=lockedContainers.mcaddon"
+set "packFileName=lockedContainers.mcpack"
 
 :: Set the output zip file name
 set "zipFileName=temp.zip"
 
-:: Delete old mcaddon
-del "%addonFileName%"
+:: Delete old mcpack
+del "%packFileName%"
 
 :: Create the tar file with exclusions
 @REM tar.exe -cvf "%tarFileName%" "manifest.json"
 tar.exe -acf "%zipFileName%" --exclude %zipFileName% --exclude ".git" --exclude %~nx0 --exclude "README.md" *
 
-:: Rename the zip file to lockedContainers.mcaddon
-rename "%zipFileName%" "%addonFileName%"
+:: Rename the zip file to lockedContainers.mcpack
+rename "%zipFileName%" "%packFileName%"
 
 endlocal
